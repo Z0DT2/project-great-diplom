c73274537a6ac0ffb53173182275c6f81a09e6e6
Z0DT2 <sergeysv527@gmail.com>
2024-04-18 13:26:37 +0300

    Co-authored-by: Misha203 <Misha203@users.noreply.github.com>

--- reset.php
+++ reset.php
@@ -2,18 +2,51 @@
 $connection_string = "host=localhost port=5432 dbname=Base user=postgres password=Sergoe";
 $db_connection = pg_connect($connection_string);
 
+$key = "aboba204980sdf";
 
-if (isset($_COOKIE['name']) && isset($_COOKIE['password']) && isset($_COOKIE['email'])) 
+$jwt = $_COOKIE['jwt'];
+$decoded_payload = jwt_decode($jwt, $key);
+$user_id = $decoded_payload['user_id'];
+
+function jwt_decode($jwt, $key) {
+    list($header, $payload, $signature) = explode('.', $jwt);
+    
+    $header = base64url_decode($header);
+    $header = json_decode($header, true);
+    
+    $payload = base64url_decode($payload);
+    $payload = json_decode($payload, true);
+    
+    $signature = base64url_decode($signature);
+    
+    $algorithm = $header['alg'];
+    // $expected_signature = hash_hmac($algorithm, "$header.$payload", $key, true);
+    $expected_signature = hash_hmac($algorithm, json_encode($header) . '.' . json_encode($payload), $key, true);
+  
+    // if ($signature === $expected_signature) {
+      return $payload;
+    // } else {
+    //   return false; // Подпись не соответствует ожидаемой
+    // }
+  }
+  
+  function base64url_decode($data) {
+    $base64 = str_replace(array("-", "_"), array("+", "/"), $data);
+    $base64 = base64_decode($base64);
+    
+    return $base64;
+  }
+
+if (isset($_COOKIE['jwt'])) 
 {
-    $email = $_COOKIE['email'];
-    $oldpasswordcookie = $_COOKIE['password'];
+    $oldpasswordcookie = $decoded_payload['password'];
     $oldpassword = $_POST['oldpassword'];
     $newpassword = $_POST['newpassword'];
     $newpasswordrep = $_POST['newpasswordrep'];
-    if ($oldpassword == $oldpasswordcookie and $newpassword == $newpasswordrep)
+    if ($oldpassword == $oldpasswordcookie && $newpassword == $newpasswordrep)
     {
-        $temppassword = pg_query($db_connection,"UPDATE users SET password = '$newpassword'  WHERE email = '$email' AND password = '$oldpassword'");
-        header("Location: /project-great-diplom/login.html");
+        $temppassword = pg_query($db_connection,"UPDATE users SET password = '$newpassword'  WHERE idusers = '$user_id' AND password = '$oldpassword'");
+        header("Location: /PFTC_/login.html");
     }
     else 
     echo '<script>alert("вы ввели неправильный старый пароль или не повторили новый.")</script>';
---
+++ бета/reset.php
@@ -0,0 +1,54 @@
+<?php
+$connection_string = "host=localhost dbname=base user=postgres password=123";
+$db_connection = pg_connect($connection_string);
+$key = "aboba204980sdf";
+
+$jwt = $_COOKIE['jwt'];
+$decoded_payload = jwt_decode($jwt, $key);
+$user_id = $decoded_payload['user_id'];
+
+function jwt_decode($jwt, $key) {
+    list($header, $payload, $signature) = explode('.', $jwt);
+    
+    $header = base64url_decode($header);
+    $header = json_decode($header, true);
+    
+    $payload = base64url_decode($payload);
+    $payload = json_decode($payload, true);
+    
+    $signature = base64url_decode($signature);
+    
+    $algorithm = $header['alg'];
+    // $expected_signature = hash_hmac($algorithm, "$header.$payload", $key, true);
+    $expected_signature = hash_hmac($algorithm, json_encode($header) . '.' . json_encode($payload), $key, true);
+  
+    // if ($signature === $expected_signature) {
+      return $payload;
+    // } else {
+    //   return false; // Подпись не соответствует ожидаемой
+    // }
+  }
+  
+  function base64url_decode($data) {
+    $base64 = str_replace(array("-", "_"), array("+", "/"), $data);
+    $base64 = base64_decode($base64);
+    
+    return $base64;
+  }
+
+if (isset($_COOKIE['jwt'])) 
+{
+    $oldpasswordcookie = $decoded_payload['password'];
+    $oldpassword = $_POST['oldpassword'];
+    $newpassword = $_POST['newpassword'];
+    $newpasswordrep = $_POST['newpasswordrep'];
+    if ($oldpassword == $oldpasswordcookie && $newpassword == $newpasswordrep)
+    {
+        $temppassword = pg_query($db_connection,"UPDATE users SET password = '$newpassword'  WHERE idusers = '$user_id' AND password = '$oldpassword'");
+        header("Location: /PFTC_/login.html");
+    }
+    else 
+    echo '<script>alert("вы ввели неправильный старый пароль или не повторили новый.")</script>';
+    echo '<script>window.location.href = "tarifs.html"</script>';
+    exit;
+}
